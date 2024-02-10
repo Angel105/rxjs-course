19921ce5d09d1ef922e51f65d73cbcfa2491908c
flyin <flying.falcon@outlook.com>
2024-02-10 16:56:20 +0100

    combine multiple types of streams by using native callbacks in a way that it creates a what's known as callback-hell by using a lot of nested code making is hard to reason about

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -28,16 +28,22 @@ export class AboutComponent implements OnInit {
   ngOnInit() {
     document.addEventListener('click', event => {
       console.log(event);
-    });
-    let counter = 0;
-    setInterval(()=>{
-      console.log(counter);
-      counter++;
-    }, 1000);
 
-    setTimeout(() => {
-      console.log("finished...");
-    },3000);
+      setTimeout(() => {
+        console.log("finished...");
+
+        let counter = 0;
+        setInterval(()=>{
+          console.log(counter);
+          counter++;
+        }, 1000);
+
+      },3000);
+
+    });
+
+
+
 
   }
 
