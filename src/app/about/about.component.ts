e08ab7ac140ff004f71c13c911c1b8528e63dea4
flyin <flying.falcon@outlook.com>
2024-02-11 22:09:05 +0100

    Observable Concatenation - In-Depth Explanation: 3 observables are concatenated using concat operator. Then, on subscribing, first observable prints its values, then the second, and finally the third.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {noop} from 'rxjs';
+import {concat, noop, of} from 'rxjs';
 import {createHttpObservable} from "../common/util";
 import {map} from "rxjs/operators";
 
@@ -12,6 +12,13 @@ import {map} from "rxjs/operators";
 export class AboutComponent implements OnInit {
 
   ngOnInit() {
+    const source1$ = of(1,2,3);
+    const source2$ = of(4,5,6);
+    const source3$ = of(7,8,9);
+
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(val => console.log(val));
 
 
   }
