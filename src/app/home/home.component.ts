6b8337c36b986fc425c80790ba2a501f5ca961f5
flyin <flying.falcon@outlook.com>
2024-02-11 17:54:55 +0100

    Building Components with RxJs - Imperative Design : apply imperative approach: taking data inside of subscribe() method is considered as imperative approach of implementing our component. (would lead to nested subscribe calls)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,50 @@
 import {Component, OnInit} from '@angular/core';
+import {createHttpObservable} from "../common/util";
+import {map} from "rxjs/operators";
+import {noop} from "rxjs";
+import {Course} from "../model/course";
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-    /*beginnerCourses$: Observable<Course[]>;
-    advancedCourses$: Observable<Course[]>;*/
+  /*beginnerCourses$: Observable<Course[]>;
+  advancedCourses$: Observable<Course[]>;*/
 
+  beginnerCourses: Course[];
+  advancedCourses: Course[];
 
-    constructor(/*private store:Store*/) {
+  constructor(/*private store:Store*/) {
 
-    }
+  }
 
-    ngOnInit() {
+  ngOnInit() {
 
-        /*const courses$ = this.store.courses$;
-        this.beginnerCourses$ = this.store.selectBeginnerCourses();
-        this.advancedCourses$ = this.store.selectAdvancedCourses();*/
+    /*const courses$ = this.store.courses$;
+    this.beginnerCourses$ = this.store.selectBeginnerCourses();
+    this.advancedCourses$ = this.store.selectAdvancedCourses();*/
 
+    const http$ = createHttpObservable('/api/courses');
 
+    const courses$ = http$
+      .pipe(
+        map(httpResponse => Object.values(httpResponse["payload"]))
+      );
 
-    }
+    courses$.subscribe(
+      courses => {
+        console.log(courses);
+        this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
+        this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
+      },
+      noop,
+      () => console.log('completed')
+    );
+
+  }
 
 }
