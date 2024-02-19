f92471e816060efd504b2a2540f03d0a16528c7c
flyin <flying.falcon@outlook.com>
2024-02-19 17:27:29 +0100

    Understanding the merge Observable combination Strategy: use interval operator to create a series of consecutive integers emitted every 1000ms as interval1$ observable, and interval2$ as a series of values of first observable multiplied by 10. Now, merge two observables to execute them in parallel, and subscribe to result$ observable to start executing the flow and log the values to console. Practical example to follow next.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {concat, interval, noop, of} from 'rxjs';
-import {createHttpObservable} from "../common/util";
+import {interval, merge} from 'rxjs';
 import {map} from "rxjs/operators";
 
 
@@ -12,15 +11,13 @@ import {map} from "rxjs/operators";
 export class AboutComponent implements OnInit {
 
   ngOnInit() {
-    const source1$ = interval(1000);
-    const source2$ = of(4,5,6);
-    const source3$ = of(7,8,9);
 
-    const result$ = concat(source1$, source2$, source3$);
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map(val => 10 * val));
 
-    // result$.subscribe(val => console.log(val));
+    const result$ = merge(interval1$, interval2$);
 
-
+    result$.subscribe(console.log);
   }
 
 }
